e9a23adefd541706b39438aa0f8e95adb6a48db4
David Jaramillo <duvan318@gmail.com>
2024-10-31 23:51:12 -0500

    Create model PromptTemplate

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class PromptTemplate(models.Model):
+    PROMPT_TYPE_CHOICES = (
+        ("CH", "Challenge"),
+        ("FE", "Feedback"),
+        ("CO", "Config"),
+    )
+    type = models.CharField(max_length=12, choices=PROMPT_TYPE_CHOICES)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
