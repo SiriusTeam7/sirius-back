7090b5447bb8e9183198ff1983ee9ed81645341e
David Jaramillo <duvan318@gmail.com>
2024-11-01 00:03:58 -0500

    Register PromptTemplate model in admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from core.models import PromptTemplate
 
-# Register your models here.
+
+class PromptTemplateAdmin(admin.ModelAdmin):
+    model = PromptTemplate
+    list_display = ("type", "created_at", "updated_at")
+
+
+admin.site.register(PromptTemplate, PromptTemplateAdmin)
