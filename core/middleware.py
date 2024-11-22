16c537c6b9b0d066b50cfdc619e01c874a75804e
david <duvan318@gmail.com>
2024-11-22 16:58:42 -0500

    Edit middleware to support token

--- core/middleware.py
+++ core/middleware.py
@@ -1,12 +1,23 @@
 from core.models import Student
 
+from core.models import Student
+from rest_framework.authentication import TokenAuthentication
+
 
 class StudentMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        if request.user.is_authenticated:
+        if not hasattr(request, "user") or not request.user.is_authenticated:
+            auth = TokenAuthentication()
+            try:
+                user_auth_tuple = auth.authenticate(request)
+                if user_auth_tuple:
+                    request.user = user_auth_tuple[0]
+            except Exception:
+                request.user = None
+        if request.user and request.user.is_authenticated:
             try:
                 request.student = request.user.student
             except Student.DoesNotExist:
